796e0da36dabaccd7840d88201ae07421e7a2fdb
Arif <arifsetyo.a1@gmail.com>
2019-04-05 15:16:46 +0700

    add dynamic form

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,50 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page, Dynamicform tryhard</h1>
+    <div v-html="formUI()"/>
   </div>
 </template>
+
+<script>
+export default {
+  name: "about",
+  components: {},
+  data() {
+    return {
+      data: [],
+      model: [
+        { key: "name", label: "Name", type: "text" },
+        { key: "age", label: "Age", type: "number" },
+        { key: "alamat", label: "Alamat", type: "textarea" }
+      ]
+    };
+  },
+  methods: {
+    formUI() {
+      let model = this.model;
+      let renderUI = model.map(m => {
+        let key = m.key;
+        let label = m.label;
+        let type = m.type;
+
+        let input = `<input class='form-input' type=${type} key=${key} />`;
+        if (type == "textarea") {
+          input = `<textarea class='form-control' key=${key}></textarea>`;
+        }
+        return `<div key=${"g" + key} class="form-group">
+            <label class="form-label" key=${"l" + key} htmlFor=${key}>
+              ${label}
+            </label>
+            ${input}
+          </div>
+        `;
+      });
+
+      return renderUI.join("");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
